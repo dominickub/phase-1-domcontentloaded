f6023936bac9878e45725f8f3068ae9550071e0a
dominick <dominickugas@gmail.com>
2021-10-18 22:27:46 -0400

    commit tree bc2d443867696bd1110a33c3f4b481a0826f4516

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Your code goes here
+// document.addEventListener("DOMContentLoaded", function () {
+//     let returningText = document.getElementById("text");
+//     returningText.textContent = "This is really cool!"
+//     console.log(returningText);
+// });
+
+
+let text = document.getElementById("text")
+
+document.addEventListener("DOMContentLoaded", () => console.log(text.textContent = "This is really cool!"))
